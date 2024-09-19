0845ddca0f594e9cd261215a40d3a6d9f4c9b879
Alessandro Lemos <ablemosjunior@gmail.com>
2024-09-19 14:56:31 -0300

    ♻️ refactor:  adjusting user asynchronism

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,11 +1,12 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { UserService } from './user.service';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+
 import { User } from './entities/user.entity';
+import { UserService } from './user.service';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
-import { UseGuards } from '@nestjs/common';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 
 @Resolver(() => User)
@@ -16,32 +17,29 @@ export class UserResolver {
   ) {}
 
   @Mutation(() => User)
-  async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+  createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
     return this.userService.create(createUserInput);
   }
 
   @Query(() => String)
-  async login(
-    @Args('email') email: string,
-    @Args('password') password: string,
-  ) {
+  login(@Args('email') email: string, @Args('password') password: string) {
     return this.authService.login(email, password);
   }
 
   @Query(() => User)
-  async findOne(@Args('email') email: string) {
+  findOne(@Args('email') email: string) {
     return this.userService.findOneByEmail(email);
   }
 
   @UseGuards(JwtAuthGuard)
   @Mutation(() => User)
-  async updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
+  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
     return this.userService.update(updateUserInput.id, updateUserInput);
   }
 
   @UseGuards(JwtAuthGuard)
   @Mutation(() => Boolean)
-  async removeUser(@Args('id') id: number, @CurrentUser() user: User) {
+  removeUser(@Args('id') id: number, @CurrentUser() user: User) {
     return this.userService.remove(id);
   }
 }
